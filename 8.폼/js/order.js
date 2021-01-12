c576ccdddbf8f9ba031e5698b01ee391894ce391
Wooyoung O <fz7948@gmail.com>
2021-01-12 19:56:43 +0900

    Add files via upload

---
+++ 8.폼/js/order.js
@@ -0,0 +1,18 @@
+var check = document.querySelector("#shippingInfo");
+
+check.addEventListener("click", function() {
+    if (check.checked == true) {
+        document.querySelector("#shippingName").value
+        =document.querySelector("#billingName").value;
+        document.querySelector("#shippingTel").value
+        =document.querySelector("#billingTel").value;
+        document.querySelector("#shippingAddr").value
+        =document.querySelector("#billingAddr").value;
+    }
+    else {
+        
+        document.querySelector("#billingName").value="";
+        document.querySelector("#billingTel").value="";
+        document.querySelector("#billingAddr").value="";
+    }
+});
